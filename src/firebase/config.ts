33a29413ea4282349bccb5df01417c8922c18e25
Divagar K <divagarask@gmail.com>
2025-11-07 10:15:17 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5229982652-cc9f0",
+  "appId": "1:436005223979:web:f5cefa0068f3fb77ee746e",
+  "apiKey": "AIzaSyBPjMWOk9VxtG7_YV-WeIr6K-cBBs4OkOw",
+  "authDomain": "studio-5229982652-cc9f0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "436005223979"
+};
