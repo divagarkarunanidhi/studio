edd4669651f9a9f21bb31e8a61d129c5bee75d42
TAAS <taasdhl@gmail.com>
2025-12-02 11:48:40 +0000

    when new user sign up they should be defaulted to view role not the taas

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -97,8 +97,7 @@ export default function LoginPage() {
       await signInWithEmailAndPassword(auth, email, password);
       toast({ title: 'Signed In!', description: 'Welcome back.' });
       router.push('/');
-    } catch (error: any) {
-      toast({
+    } catch (error: any)      toast({
         variant: 'destructive',
         title: 'Sign In Failed',
         description: error.message,
